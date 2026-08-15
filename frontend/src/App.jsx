16b10205747b8c04e8567b2d18268529fd608da5
renimodi <modireni4218@gmail.com>
2026-08-15 20:50:16 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AppProvider } from "./context/AppContext";
+
 import HomePage from "./pages/HomePage";
 import DashboardPage from "./pages/DashboardPage";
+import MarketPage from "./pages/MarketPage";
+import BuyersPage from "./pages/BuyersPage";
+import AdvisorPage from "./pages/AdvisorPage";
 
 function App() {
   return (
@@ -10,6 +14,9 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/market" element={<MarketPage />} />
+          <Route path="/buyers" element={<BuyersPage />} />
+          <Route path="/advisor" element={<AdvisorPage />} />
         </Routes>
       </BrowserRouter>
     </AppProvider>
